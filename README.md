bf8b2296faebbfb3b17db5e883e956ee3d8b0975
Erik Israel Romero Quispe <midas-claw@Eriks-Mac-mini.local>
2018-11-12 15:08:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Todo-List
+
+Start the app
+
+```bash
+$ npm start
+
+Server is running at http://127.0.0.1:3000
+```
---
+++ test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {Lb4TodoListApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new Lb4TodoListApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: Lb4TodoListApplication;
+  client: Client;
+}
